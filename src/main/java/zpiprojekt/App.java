81c097f16070b4f956f60119dab72df43e2a09b1
adriankucharski <adrian1998.25@live.com>
2021-03-23 16:31:23 +0100

    first commit

---
+++ src/main/java/zpiprojekt/App.java
@@ -0,0 +1,13 @@
+package zpiprojekt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
