22491b8e8374c21ed0599fb4f6df97408e4013dc
Bartek306 <patoraban3@wp.pl>
2021-03-23 17:19:10 +0100

    Add display line

--- src/main/java/zpiprojekt/App.java
+++ src/main/java/zpiprojekt/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "\tGwiezdna Flota\nAdrian Kucharski - tester\nGithubId: adriankucharski" );
+        System.out.println("Bartosz Maluszczak - GithubID: 222593");
+
     }
 }
