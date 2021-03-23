e0f159a521946af669820c4ca60e27c93a921ede
Whitesan <piotrekrek12@wp.pl>
2021-03-23 17:19:52 +0100

    Print name and git id

--- src/main/java/zpiprojekt/App.java
+++ src/main/java/zpiprojekt/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "\tGwiezdna Flota\nAdrian Kucharski - tester\nGithubId: adriankucharski" );
+        System.out.println("Piotr Misztal - GithubID: 222518");
+
     }
 }
