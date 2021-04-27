acc5febcab8000840365bfc3a894795211eb25be
adriankucharski <adrian1998.25@live.com>
2021-04-27 16:43:15 +0200

    clear commit

--- src/main/java/zpiprojekt/App.java
+++ src/main/java/zpiprojekt/App.java
@@ -1,18 +1,9 @@
 package zpiprojekt;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        //Wyświetlam imie nazwisko role i Github id
-        System.out.println( "\tGwiezdna Flota\nAdrian Kucharski - tester\nGithubId: adriankucharski" );
-        System.out.println("Piotr Misztal - GithubID: 222518");
-        System.out.println("Adrian Kucharski - GithubID: adriankucharski");
-        System.out.println("Bartosz Maluszczak - GithubID: Bartek306");
-        System.out.println("Bartlomiej Świstak - GithubID: 222593");
+        
     }
 }
