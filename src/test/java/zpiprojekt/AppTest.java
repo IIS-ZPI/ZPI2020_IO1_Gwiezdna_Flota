64e06f672cbbe6674cb22606cec23f86583b273f
adriankucharski <adrian1998.25@live.com>
2021-05-24 22:08:14 +0200

    added actionSession, first input tests

--- src/test/java/zpiprojekt/AppTest.java
+++ src/test/java/zpiprojekt/AppTest.java
@@ -2,6 +2,11 @@ package zpiprojekt;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import org.junit.Test;
 
 public class AppTest 
@@ -10,6 +15,32 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+
+    private int testingUserInput(String inputText){
+        InputStream in = new ByteArrayInputStream(inputText.getBytes());
+
+        // Make backup of System.in
+        InputStream systemInBackup = System.in;
+
+        System.setIn(in);
+        Scanner sc = new Scanner(System.in);
+        String nextLine = sc.nextLine();
+        sc.close();
+
+        // Restore original System.in
+        System.setIn(systemInBackup);
+
+        return inputText.compareTo(nextLine);
+    }
+
+    @Test
+    public void testingUserInputText(){
+        String[] words = {"ads", "dabv", "12332t", "dfvhheeyut", "zxvvbcmn"};
+
+        for(int i = 0; i < words.length; ++i)
+            assertTrue(testingUserInput(words[i]) == 0);
     }
 
 }
