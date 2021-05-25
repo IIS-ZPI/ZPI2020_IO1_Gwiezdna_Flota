ded71724d5d643d5f06e745d8e5f1c10fc20210a
Bartek306 <shwdp13@wp.pl>
2021-05-25 18:14:24 +0100

    Add ActionDistribution class

--- src/main/java/zpiprojekt/nbp/data/RateTable.java
+++ src/main/java/zpiprojekt/nbp/data/RateTable.java
@@ -1,6 +1,8 @@
 package zpiprojekt.nbp.data;
 
-import java.util.ArrayList;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class RateTable {
 	public String table;
@@ -15,4 +17,29 @@ public class RateTable {
 				"code: "+code+"\n"+
 				"rates:\n"+rates.toString();
 	}
+
+	public HashMap<String, Double> getChangeTable(){
+		Collections.sort(rates, new Comparator<Rate>() {
+			@Override public int compare(Rate rate1, Rate rate2) {
+				SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+				Date date1 = null;
+				Date date2 = null;
+				try {
+					date1 = format.parse(rate1.effectiveDate);
+					date2 = format.parse(rate2.effectiveDate);
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+
+				return date1.compareTo(date2);
+			}
+		});
+		LinkedHashMap<String, Double> hashMap = new LinkedHashMap<>();
+		for(int i=1; i<rates.size(); i++){
+			Double diff = (rates.get(i).mid - rates.get(i-1).mid) / rates.get(i -1).mid * 100;
+			hashMap.put(rates.get(i).effectiveDate, Math.abs(diff));
+		}
+		return hashMap;
+	}
 }
+
