36ab53ea3255cf44784dd65dc445057caa1432ea
adriankucharski <adrian1998.25@live.com>
2021-05-25 18:18:15 +0200

    Unit test of UI

--- src/test/java/zpiprojekt/AppTest.java
+++ src/test/java/zpiprojekt/AppTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 import org.junit.Test;
@@ -43,4 +42,225 @@ public class AppTest
             assertTrue(testingUserInput(words[i]) == 0);
     }
 
+
+    private InputStream writeStringAsSystemIn(String toWrite){
+        // Create new InputStream that write string
+        InputStream in = new ByteArrayInputStream(toWrite.getBytes());
+        return in;
+    }   
+
+    /**
+    * * * * * * * * * * * * * * * * * * * * * * * * *
+    * UIClient.actionDistributionChanges
+    * * * * * * * * * * * * * * * * * * * * * * * * *
+    */
+    @Test 
+    public void testUIClientActionDistributionChanges001(){
+        String toWrite = "3413t2af";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionDistributionChanges();
+
+        assertTrue(retVal == 1);
+    }
+
+    @Test 
+    public void testUIClientActionDistributionChanges002(){
+        String toWrite = "EUR\nUSD\n1";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionDistributionChanges();
+
+        assertTrue(retVal == 0);
+    }
+
+    @Test 
+    public void testUIClientActionDistributionChanges003(){
+        String toWrite = "PLN";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionDistributionChanges();
+
+        assertTrue(retVal == 1);
+    }
+
+    @Test 
+    public void testUIClientActionDistributionChanges004(){
+        String toWrite = "EUR\nPLN";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionDistributionChanges();
+
+        assertTrue(retVal == 1);
+    }
+
+    @Test 
+    public void testUIClientActionDistributionChanges005(){
+        String toWrite = "EUR\nEUR";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionDistributionChanges();
+
+        assertTrue(retVal == 1);
+    }
+
+    @Test 
+    public void testUIClientActionDistributionChanges006(){
+        String toWrite = "EUR\nPLN";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionDistributionChanges();
+
+        assertTrue(retVal == 1);
+    }
+
+    @Test 
+    public void testUIClientActionDistributionChanges007(){
+        String toWrite = "EUR\nUSD\n0";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionDistributionChanges();
+
+        assertTrue(retVal == 3);
+    }
+
+    @Test 
+    public void testUIClientActionDistributionChanges008(){
+        String toWrite = "EUR\nUSD\n435346374";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionDistributionChanges();
+
+        assertTrue(retVal == 2);
+    }
+
+
+    /**
+    * * * * * * * * * * * * * * * * * * * * * * * * *
+    * UIClient.actionStatisticalMeasures
+    * * * * * * * * * * * * * * * * * * * * * * * * *
+    */
+    @Test 
+    public void testUIClientActionStatisticalMeasures001(){
+        String toWrite = "PLN";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionStatisticalMeasures();
+
+        assertTrue(retVal == 1);
+    }
+
+    @Test 
+    public void testUIClientActionStatisticalMeasures002(){
+        String toWrite = "USD\n1";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionStatisticalMeasures();
+
+        assertTrue(retVal == 0);
+    }
+
+    @Test 
+    public void testUIClientActionStatisticalMeasures003(){
+        String toWrite = "USD\n-1";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionStatisticalMeasures();
+
+        assertTrue(retVal == 2);
+    }
+    @Test 
+    public void testUIClientActionStatisticalMeasures004(){
+        String toWrite = "USD\n5";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionStatisticalMeasures();
+
+        assertTrue(retVal == 0);
+    }
+
+    @Test 
+    public void testUIClientActionStatisticalMeasures005(){
+        String toWrite = "USD\n13456";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionStatisticalMeasures();
+
+        assertTrue(retVal == 3);
+    }
+
+    @Test 
+    public void testUIClientActionStatisticalMeasures006(){
+        String toWrite = "";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionStatisticalMeasures();
+
+        assertTrue(retVal == 1);
+    }
+
+
+    /**
+    * * * * * * * * * * * * * * * * * * * * * * * * *
+    * UIClient.actionSessions
+    * * * * * * * * * * * * * * * * * * * * * * * * *
+    */
+    @Test 
+    public void testUIClientActionSessions001(){
+        String toWrite = "PLN";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionSessions();
+
+        assertTrue(retVal == 1);
+    }
+
+    @Test 
+    public void testUIClientActionSessions002(){
+        String toWrite = "USD\n1";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionSessions();
+
+        assertTrue(retVal == 0);
+    }
+
+    @Test 
+    public void testUIClientActionSessions003(){
+        String toWrite = "USD\n-1";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionSessions();
+
+        assertTrue(retVal == 3);
+    }
+    @Test 
+    public void testUIClientActionSessions004(){
+        String toWrite = "USD\n5";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionSessions();
+
+        assertTrue(retVal == 0);
+    }
+
+    @Test 
+    public void testUIClientActionSessions005(){
+        String toWrite = "USD\n13456";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionSessions();
+
+        assertTrue(retVal == 3);
+    }
+
+    @Test 
+    public void testUIClientActionSessions006(){
+        String toWrite = "";
+        InputStream in = writeStringAsSystemIn(toWrite);
+        UIClient.setScannerIn(in);
+        int retVal = UIClient.actionSessions();
+
+        assertTrue(retVal == 1);
+    }
 }
